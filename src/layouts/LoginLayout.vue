fbd2b8643901ba8ce26a7d4897a23fdb416e55bf
acomma <acomma@yeah.net>
2022-11-28 19:53:53 +0800

    一个简单的用户登录页面

---
+++ src/layouts/LoginLayout.vue
@@ -0,0 +1,47 @@
+<script setup lang="ts">
+import { NForm, NFormItem, NInput, NButton, NIcon } from 'naive-ui'
+import { PersonOutline, LockClosedOutline } from '@vicons/ionicons5'
+</script>
+
+<template>
+    <div class="login">
+        <div class="container">
+            <n-form label-placement="left">
+                <n-form-item>
+                    <n-input placeholder="请输入用户名">
+                        <template #prefix>
+                            <n-icon :component="PersonOutline" />
+                        </template>
+                    </n-input>
+                </n-form-item>
+                <n-form-item>
+                    <n-input placeholder="请输入密码">
+                        <template #prefix>
+                            <n-icon :component="LockClosedOutline" />
+                        </template>
+                    </n-input>
+                </n-form-item>
+                <n-form-item>
+                    <n-button type="primary" block>登录</n-button>
+                </n-form-item>
+            </n-form>
+        </div>
+    </div>
+</template>
+
+<style scoped>
+.login {
+    display: flex;
+    flex-direction: column;
+    height: 100vh;
+    overflow: auto;
+}
+
+.login .container {
+    flex: 1;
+    padding: 32px 12px;
+    max-width: 384px;
+    min-width: 320px;
+    margin: 0 auto;
+}
+</style>
--- src/router/index.ts
+++ src/router/index.ts
@@ -3,9 +3,14 @@ import Role from "../views/role/index.vue";
 import Menu from "../views/menu/index.vue";
 import MenuAction from "../views/menu-action/index.vue";
 import BasicLayout from "../layouts/BasicLayout.vue"
+import LoginLayout from "../layouts/LoginLayout.vue"
 import { createRouter, createWebHashHistory } from 'vue-router'
 
 const routes = [
+    {
+        path: '/login',
+        component: LoginLayout
+    },
     {
         path: '/',
         component: BasicLayout,
@@ -33,6 +38,19 @@ const routes = [
 const router = createRouter({
     history: createWebHashHistory(),
     routes
+})
+
+router.beforeEach((to, from, next) => {
+    if (to.path === '/login') {
+        next()
+    } else {
+        const token = localStorage.getItem('token')
+        if (token) {
+            next()
+        } else {
+            next({ path: '/login' })
+        }
+    }
 })
 
 export default router
